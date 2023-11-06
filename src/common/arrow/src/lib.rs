a1d6b2894d4ce99cb4f381c2550e43008fa86cc1
baishen <baishen2009@gmail.com>
2023-11-06 15:03:25 +0800

    tmp

--- src/common/arrow/src/lib.rs
+++ src/common/arrow/src/lib.rs
@@ -16,12 +16,12 @@ mod parquet_read;
 mod parquet_write;
 pub mod schema_projection;
 
-pub use arrow2;
-pub use native2;
+pub mod arrow;
+pub mod native;
 
-pub use arrow;
+//pub use arrow;
 pub use arrow_format;
-pub use native;
+//pub use native;
 pub use parquet2 as parquet;
 pub use parquet_read::read_columns_async;
 pub use parquet_read::read_columns_many_async;
